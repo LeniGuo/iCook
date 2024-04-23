d8ee6f1e272c0d3c79a7ef079c3e595b74e426c5
Leni <1153600546@qq.com>
2024-04-23 16:41:13 +0800

    change methods of downloading pretrained llm

--- demo/cli_demo.py
+++ demo/cli_demo.py
@@ -2,7 +2,7 @@ import torch
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
 
-model_name_or_path = "model/zhangxiaobai_shishen2_full"
+model_name_or_path = "model/zhanghuiATchina/zhangxiaobai_shishen2_full"
 
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, trust_remote_code=True)
 model = AutoModelForCausalLM.from_pretrained(model_name_or_path, trust_remote_code=True, torch_dtype=torch.bfloat16, device_map='auto')
--- script/download_pretrained_llm.sh
+++ script/download_pretrained_llm.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Install git-lfs: https://zhuanlan.zhihu.com/p/146683392
+apt install git-lfs
+apt update
+git lfs install
+
 # Change directory to /model
-cd ./model
+cd model
+mkdir zhanghuiATchina
+cd zhanghuiATchina
 
 # Clone the repository
 git clone https://www.modelscope.cn/zhanghuiATchina/zhangxiaobai_shishen2_full.git
-
-rm zhangxiaobai_shishen2_full/pytorch_model.bin.index.json
-
-curl -o "zhangxiaobai_shishen2_full/pytorch_model.bin.index.json" "https://www.modelscope.cn/api/v1/models/zhanghuiATchina/zhangxiaobai_shishen2_full/repo?Revision=master&FilePath=pytorch_model.bin.index.json"
