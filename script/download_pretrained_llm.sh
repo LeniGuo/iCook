c083b69016ee1f669c82c7ddcc2a952f7d49adab
Leni <1153600546@qq.com>
2024-04-23 14:44:04 +0800

    replace pytorch_model.bin.index.json

--- script/download_pretrained_llm.sh
+++ script/download_pretrained_llm.sh
@@ -5,3 +5,7 @@ cd ./model
 
 # Clone the repository
 git clone https://www.modelscope.cn/zhanghuiATchina/zhangxiaobai_shishen2_full.git
+
+rm zhangxiaobai_shishen2_full/pytorch_model.bin.index.json
+
+curl -o "zhangxiaobai_shishen2_full/pytorch_model.bin.index.json" "https://www.modelscope.cn/api/v1/models/zhanghuiATchina/zhangxiaobai_shishen2_full/repo?Revision=master&FilePath=pytorch_model.bin.index.json"
